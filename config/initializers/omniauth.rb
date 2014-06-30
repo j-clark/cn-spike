bf30d3190431202ef92b5f8bc5531002b6e1a179
Joshua Clark <pair+jclark@pivotallabs.com>
2014-06-30 15:06:43 -0400

    Load facebook id and secret from ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-  provider :facebook, '1496254523924869', '2064cdfa2724cc9993b3268483a8c4c6'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 end
