0776c7a2927cf4a8efcdc3e8c4b1a8406e6a5853
Joshua Clark <pair+jclark@pivotallabs.com>
2014-06-30 14:46:13 -0400

    User hash to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   class << self
     def create_with_omniauth(auth)
-      create! do |user|
-        user.provider = auth['provider']
-        user.uid = auth['uid']
-        user.name = auth['info']['name']
-      end
+      create(
+        provider: auth['provider'],
+        uid: auth['uid'],
+        name: auth['info']['name']
+      )
     end
   end
 end
